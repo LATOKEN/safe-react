9fe6d9f896cbf5ef815c0b8e08d1f467b1ec0530
andreyc21 <st21net@mail.ru>
2022-11-06 22:08:41 +0300

    test

--- src/logic/safe/utils/safeVersion.ts
+++ src/logic/safe/utils/safeVersion.ts
@@ -91,6 +91,10 @@ export const getSafeVersionInfo = async (safeVersion: string): Promise<SafeVersi
 export const isValidMasterCopy = async (chainId: string, masterCopyAddress: string): Promise<boolean> => {
   const supportedMasterCopies = await getMasterCopies(chainId)
 
+  logError(Errors._606, "start dump")
+  logError(Errors._606, chainId)
+  logError(Errors._606, supportedMasterCopies)
+  logError(Errors._606, masterCopyAddress)
   return supportedMasterCopies.some((supportedMasterCopy) =>
     sameAddress(supportedMasterCopy.address, masterCopyAddress),
   )
